fc0a99d1c4ff709c5c3e1bd3496ae54691c60e95
JucarBM <juanc.barbaran@gmail.com>
2023-06-27 16:23:18 -0500

    fix ci_cd

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def update_model(model: Pipeline) -> None:
     dump(model, 'model/model.pkl')
+
 
 def save_simple_model_report(train_score: float, test_score: float, validation_score: float, model: Pipeline) -> None:
 
@@ -15,11 +17,12 @@ def save_simple_model_report(train_score: float, test_score: float, validation_s
         for key, value in model.named_steps.items():
             report_file.write(f"### {key}:{value.__repr__()}\n")
 
-        report_file.write(f"###Train score: {train_score}\n")
-        report_file.write(f"###Test score: {test_score}\n")
-        report_file.write(f"###Validation score: {validation_score}\n")
+        report_file.write(f"### Train score: {train_score}\n")
+        report_file.write(f"### Test score: {test_score}\n")
+        report_file.write(f"### Validation score: {validation_score}\n")
 
-def get_model_performance_test_set (y_test: pd.DataFrame, y_test_pred: pd.DataFrame) -> None:
+
+def get_model_performance_test_set(y_test: pd.DataFrame, y_test_pred: pd.DataFrame) -> None:
 
     fig, ax = plt.subplots()
     fig.set_figheight(10)
