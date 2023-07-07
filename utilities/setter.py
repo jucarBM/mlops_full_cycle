7e72a1bd9f840adf3380bc23964c843772d17aec
JucarBM <juanc.barbaran@gmail.com>
2023-07-07 15:45:22 -0500

    CI CD finished v3

--- utilities/setter.py
+++ utilities/setter.py
@@ -4,9 +4,9 @@ from base64 import b64decode
 
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    with open('service_account.json', 'w') as json_file:
-        json_file.write(b64decode(key).decode('utf-8'))
-        print(os.path.realpath('service_account.json'))
+    with open('path.json', 'w') as json_file:
+        json_file.write(b64decode(key).decode())
+    print(os.path.realpath('path.json'))
 
 
 if __name__ == '__main__':
